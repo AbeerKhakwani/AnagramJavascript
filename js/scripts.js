aee2b70c49b6f57e4fce4a7959e08676406f1595
Abeer Khakwani <abeerkhakwani@gmail.com>
2015-04-07 16:45:06 -0700

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
+var anagram = function(word, string) {
 
-var anagramFunc= function(word,word2){
+    var stringArray = string.split(" ");
+    var returnString = "";
 
+    var WordLetters = word.split("");
+    WordLetters.sort();
+    var arrayoflettersword = [];
+    stringArray.forEach(function(string1, index) {
+                var stringLetters = string1.split("");
+                stringLetters.sort();
+                if (stringLetters.length === WordLetters.length) {
+                    for (var i = 0; stringLetters.length > i; i++) {
+                        if (stringLetters[i] === WordLetters[i]) {
+                            arrayoflettersword.push(stringLetters[i]);
+
+                        } else {
+                            return;
+                        }
+                    }
+            }
+    });
+
+    var joinedArray= arrayoflettersword.join("");
+    console.log("Word " + joinedArray);
+    return joinedArray;
 
 }
+
+
+// if( stringLetters === WordLetters){
+//
+//    returnString+="match";
+//
+// }
+// else{
+//     returnString+="Nope";
+//
+//
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,19 @@
-describe('titleCase', function() {
+describe('anagramChecker', function() {
     it("checks to see if one letter word matches", function() {
-        expect(titleCaseThree("a")).to.equal("a");
+        expect(anagram("a","a")).to.equal("match");
+    });
+
+    it("checks to see if 2 letter word matches", function() {
+        expect(anagram("ab","ab")).to.equal("match");
+    });
+    it("checks to see if 2 letter word does not matches", function() {
+        expect(anagram("ab","ac")).to.equal("Nope");
+    });
+    it("checks to see if 2 words match the original word", function() {
+        expect(anagram("ab","ac ab")).to.equal("ab");
+    });
+    it("checks to see if word in a diffrent order matchs the original word", function() {
+        expect(anagram("ab","ba")).to.equal("ab");
     });
 
 });
